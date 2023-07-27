364897bd798cf850ce57cbb9b8c805a43eece0d3
Guilherme Tramontano <tramontano.guilherme@yahoo.com.br>
2023-07-27 22:05:55 +0000

    feat: updating youtube module to be user-friendly

--- src/Handlers/Youtube/index.ts
+++ src/Handlers/Youtube/index.ts
@@ -2,64 +2,80 @@ import { createHandlerInstance, createMethod } from 'kozz-handler-maker';
 import * as YoutubeApi from 'src/API/YoutubeAPI';
 
 const firstVideo = createMethod({
-	name: 'first-video',
+	name: 'video',
 	args: {},
 	func: async requester => {
-		const query = requester.rawCommand.immediateArg;
-		if (!query) {
-			return requester.reply('No immediate arg');
+		try {
+			const query = requester.rawCommand.immediateArg;
+			if (!query) {
+				return requester.reply('No immediate arg');
+			}
+
+			const results = await YoutubeApi.searchResults(query);
+			if (!results) {
+				return requester.reply('No results');
+			}
+
+			requester.react('⏳');
+			const mediaPath = await YoutubeApi.downloadVideoFromUrl(
+				results.results[0].link
+			);
+			if (!mediaPath) {
+				return requester.reply('erro');
+			}
+
+			requester.react('🎥');
+			requester.reply.withMedia.fromPath(
+				mediaPath,
+				'video',
+				`🎥 ${results.results[0].title}`
+			);
+		} catch (e) {
+			requester.reply.withTemplate('Error', { error: e });
 		}
-
-		const results = await YoutubeApi.searchResults(query);
-		if (!results) {
-			return requester.reply('No results');
-		}
-
-		console.log('Querying video' + query + 'got result', {
-			result: results.results[0],
-		});
-
-		const mediaPath = await YoutubeApi.downloadVideoFromUrl(results.results[0].link);
-		if (!mediaPath) {
-			return requester.reply('erro');
-		}
-
-		requester.reply.withMedia.fromPath(
-			mediaPath,
-			'video',
-			`🎥 ${results.results[0].title}`
-		);
 	},
 });
 
 const firstSong = createMethod({
-	name: 'first-song',
+	name: 'song',
 	args: {},
 	func: async requester => {
-		const query = requester.rawCommand.immediateArg;
-		if (!query) {
-			return requester.reply('No immediate arg');
+		try {
+			const query = requester.rawCommand.immediateArg;
+			if (!query) {
+				return requester.reply.withTemplate('EmptyQuery');
+			}
+
+			const results = await YoutubeApi.searchResults(query);
+			if (!results) {
+				return requester.reply.withTemplate('NoResults');
+			}
+
+			requester.react('⏳');
+			const mediaPath = await YoutubeApi.downloadMp3FromUrl(results.results[0].link);
+			if (!mediaPath) {
+				return requester.reply.withTemplate('Error', {
+					error: 'Falha ao salvar o arquivo',
+				});
+			}
+
+			requester.react('🎶');
+			requester.reply.withMedia.fromPath(
+				mediaPath,
+				'audio/webm',
+				`🎥 ${results.results[0].title}`
+			);
+		} catch (e) {
+			requester.reply.withTemplate('Error', { error: e });
 		}
+	},
+});
 
-		const results = await YoutubeApi.searchResults(query);
-		if (!results) {
-			return requester.reply('No results');
-		}
-
-		console.log('Querying song' + query + 'got result', {
-			result: results.results[0],
-		});
-
-		const mediaPath = await YoutubeApi.downloadMp3FromUrl(results.results[0].link);
-		if (!mediaPath) {
-			return requester.reply('erro');
-		}
-
-		requester.reply.withMedia.fromPath(
-			mediaPath,
-			'audio/webm',
-			`🎥 ${results.results[0].title}`
-		);
+const help = createMethod({
+	name: 'fallback',
+	args: {},
+	func: async requester => {
+		requester.reply.withTemplate('Help');
 	},
 });
 
@@ -71,5 +87,7 @@ export const startYoutubeHandler = () =>
 		methods: {
 			...firstSong,
 			...firstVideo,
+			...help,
 		},
+		templatePath: 'src/Handlers/Youtube/messages.kozz.md',
 	});
---
+++ src/Handlers/Youtube/messages.kozz.md
@@ -0,0 +1,33 @@
+> @Help
+
+# Youtube no zap!
+
+Esse módulo pesquisa no youtube e baixa o primeiro resultado que encontrar.
+<br>
+!yt song (pesquisa) para baixar como música
+!yt video (pesqisa) para baixar como vídeo
+<br>
+Reações:
+✅: Comando validado
+⏳: Baixando, aguarde
+🎶 ou 🎥: Midia baixada, enviando
+
+> ---
+
+> @EmptyQuery
+
+Sua pesquisa não pode ser vazia.
+
+> ---
+
+> @NoResults
+
+Putz, não achei nada. Tente refinar sua pesquisa.
+
+> ---
+
+> @Error
+
+Erro: {{error}}
+
+> ---
